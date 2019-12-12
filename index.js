0459030ed6b0976ba75e13a9063afe809d2da902
Justin Bermudez <justinanbermudez@gmail.com>
2019-12-12 20:26:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ function currentLine(line){
   if(line.length === 0){
     return('The line is currently empty.');
   }
+  // best way to return line?
   var lineNameAndPlace = [];
 
   for(let i = 0; i < line.length; i++){
